327abefd78f8d6d1a1a2d0bd364d82ad1886b21b
George Lynch <osu4life75@gmail.com>
2024-07-21 11:52:09 -0500

    Update GameBody.tsx

--- mine-sweeper/src/components/GameBody.tsx
+++ mine-sweeper/src/components/GameBody.tsx
@@ -2,42 +2,32 @@ import React from "react";
 import Tile from "./Tile";
 
 export default function GameBody() {
-    let t:string[] = [];
-    function populateTiles(limit:number=60) {
-        console.log("populate", limit)
-        for (let i = 0; i < limit; i++) {
-          t.push("t");
-        }
-      }
- 
-
-
+  let t: string[] = [];
+  function populateTiles(limit: number = 60) {
+    console.log("populate", limit);
+    for (let i = 0; i < limit; i++) {
+      t.push("t");
+    }
+  }
 
   return (
     <>
-   
-        
-   
-        <div className="row">
-            <div className="col-12">
-            <button onClick={() => populateTiles(60)}>Add Tiles</button>
-
-            </div>
+      <div className="row">
+        <div className="col-12">
+          <button onClick={() => populateTiles(60)}>Add Tiles</button>
         </div>
-        <div className="row">
-            {
-
-                t && t.length > 0 ?
-                t.map(tile=>(
-                    <div>
-                <Tile/>
+      </div>
+      <div className="row">
+        {t && t.length > 0 ? (
+          t.map((Tile) => (
+            <div>
+              <Tile/>
             </div>
-                    
-                ))
-                 : (<div>no tiles</div>)
-            }
-            
-        </div>
-</>
+          ))
+        ) : (
+          <div>no tiles</div>
+        )}
+      </div>
+    </>
   );
 }
